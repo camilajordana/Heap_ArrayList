e2cfc51103108ad28be3488f512542f087a889f1
Camila Jordana Ribeiro Teixeira <c_jordana@hotmail.com>
2016-07-06 18:18:44 -0300

    Update Heap.java

--- src/Heap.java
+++ src/Heap.java
@@ -1,73 +1,85 @@
 
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class Heap {
-
-    
-    // 1º: cria um arrayList
-    
-    private ArrayList<Integer> elementos = new ArrayList();
-
-    private int qtd;
-    
-    public Heap(){
-        //Adicionando 0 ao primeiro elemento do array.
+    private List<Integer> elementos = new ArrayList();
+    int quantidade = 0;
+    public Heap() {
+        // A primeira posição do array é responsável por guardar a quantidade de 
+        // elementos do array
         elementos.add(0);
     }
-    
-    public void inserir(Integer key){
-        elementos.add(key);
-        qtd++;
-        elementos.set(0, qtd);
-        upheap(qtd);
+
+    public void inserir(Integer chave) {
+        elementos.add(chave);
+        quantidade++;
+        elementos.set(0, quantidade);
+        upheap(quantidade);
     }
-    
-    public void upheap(int indice){
-        //System.out.println(indice);
-        while(indice != 1){
-            if(elementos.get(indice) < elementos.get(indice/2)){
-                troca(indice, indice/2);
-                indice = indice/2;
+
+    public void upheap(int indice) {
+        while (indice != 1) {
+            if (elementos.get(indice) < elementos.get(indice / 2)) {
+                troca_Pai_E_Filho(indice, indice / 2);
+                indice = indice / 2;
                 upheap(indice);
             }
             break;
         }
     }
-    
-    public void troca(int indice_pai, int indice_filho){
-        Integer auxiliar = elementos.get(indice_filho);
-        elementos.set(indice_filho, elementos.get(indice_pai));
-        elementos.set(indice_pai, auxiliar);
+
+    public void troca_Pai_E_Filho(int indiceFilho, int indicePai) {
+        Integer auxiliar = elementos.get(indiceFilho);
+        elementos.set(indiceFilho, elementos.get(indicePai));
+        elementos.set(indicePai, auxiliar);
     }
-    
-    
-    public void exibir(){
-        for(int i = 1; i <= qtd; i++) System.out.print(elementos.get(i) + " ");
-    }
-    
-    public void remover(){
-        elementos.set(1, elementos.get(qtd));
-        elementos.remove(qtd);
-        qtd--;
-        elementos.set(0, qtd);
-        downheap(1);
-    }
-    
-    public void downheap(int indice){
-        if(indice*2 < qtd){
-            int indice_do_pai_atual = elementos.get(indice);
-            int indice_do_filho_esquerdo = elementos.get(indice*2);
-            int indice_do_filho_direito = elementos.get(indice*2+1);
-            if(indice_do_pai_atual > indice_do_filho_esquerdo && indice_do_filho_esquerdo < indice_do_filho_direito){
-                //troca(indice_do_pai_atual, indice_do_filho_esquerdo);
-                troca(indice*2, indice);
-                downheap(indice*2);
-            } else if(indice_do_pai_atual > indice_do_filho_direito && indice_do_filho_direito < indice_do_filho_esquerdo){
-                //troca(indice_do_pai_atual, indice_do_filho_direito);
-                troca(indice*2+1, indice);
-                downheap(indice*2+1);
-            }
+
+    public void removeMin() {
+        elementos.set(1, elementos.get(quantidade));
+        elementos.remove(quantidade);
+        quantidade--;
+        elementos.set(0, quantidade);
+        if(quantidade != 0){
+            downheap(1);
+        } else {
+            System.out.println("Todos os elementos foram removidos");
         }
     }
-    
+
+    public void downheap(int indice) {
+
+        if (quantidade != indice) { // Se tiver filhos
+            if (quantidade != indice * 2 + 1) { // Caso não tenho filho direito
+                int indice_do_pai_atual = elementos.get(indice);
+                int indice_do_filho_esquerdo = elementos.get(indice * 2);
+                if (indice_do_pai_atual > indice_do_filho_esquerdo) {
+                    troca_Pai_E_Filho(indice * 2, indice);
+                    downheap(indice * 2);
+                }
+            } else {
+                int indice_do_pai_atual = elementos.get(indice);
+                int indice_do_filho_esquerdo = elementos.get(indice * 2);
+                int indice_do_filho_direito = elementos.get(indice * 2 + 1);
+                if (indice_do_pai_atual > indice_do_filho_esquerdo && indice_do_filho_esquerdo < indice_do_filho_direito) {
+                    //troca(indice_do_pai_atual, indice_do_filho_esquerdo);
+                    troca_Pai_E_Filho(indice * 2, indice);
+                    downheap(indice * 2);
+                } else if (indice_do_pai_atual > indice_do_filho_direito && indice_do_filho_direito < indice_do_filho_esquerdo) {
+                    //troca(indice_do_pai_atual, indice_do_filho_direito);
+                    troca_Pai_E_Filho(indice * 2 + 1, indice);
+                    downheap(indice * 2 + 1);
+                }
+            }
+        }
+
+    }
+
+    public void exibirArray() {
+        for (int i = 1; i <= quantidade; i++) {
+            System.out.print(elementos.get(i) + " ");
+        }
+    }
+
 }
